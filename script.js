0f1f3c4a1dbc20f24ca8b66201f19ce3f836cc3c
Asha Luckins <ashaluckinsanoop@gmail.com>
2024-05-16 16:56:35 -0400

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("testing");
+const videoElement=document.getElementById("video");
+const button=document.getElementById("button");
+
+
+//Prompt to select media stream , pass the video elements and play
+
+
+async function selectMediaStream(){
+    try{
+        const mediaStream=await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject=mediaStream;
+        videoElement.onloadedmetadata=()=>{
+            videoElement.play();
+        }
+    }catch(error){
+        console.log("whoops!error:", error);
+    }
+}
+
+button.addEventListener('click',async ()=>{
+    button.disabled=true;
+    await videoElement.requestPictureInPicture();
+    button.disabled=false;
+})
+
+//On load
+selectMediaStream();
